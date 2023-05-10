810ba1741d01a65fbc587288af144dd4fcda39df
Vanshika <vanshikav928@gmail.com>
2023-05-10 16:21:47 +0530

    rest operator

---
+++ Rest_operator.js
@@ -0,0 +1,26 @@
+/*function sum(){
+    console.log(arguments);
+
+    let sum=0;
+    for(let i in arguments){
+        sum+=arguments[i];
+    }
+
+    console.log(sum);
+}
+sum("Vanshika",20,30,40);*/
+
+function sum(name,...args){
+    console.log(arguments);
+
+    console.log(`Hello ${name} :`);
+
+    let sum=0;
+    for(let i in args){
+        sum+=args[i];
+    }
+
+    console.log(sum);
+}
+sum("Vanshika",20,30,40)
+
