dbd0f7cc29115160cce1dc7036180058170c80c7
Vanshika <vanshikav928@gmail.com>
2023-05-10 14:18:08 +0530

    arrow_functions

---
+++ arrow_functions.js
@@ -0,0 +1,12 @@
+/*let welcome = function(name){
+    return` Hello & Welcome  ${name}`;
+}
+
+console.log(welcome("vanshika"));*/
+
+
+let welcome =(name,age)=>{
+    return `Hello &  Welcome ${name} - ${age}`;
+}
+
+console.log(welcome("Vanshika",19));
