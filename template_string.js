92ad4eda4536a630cbc59a511fa8c63bb1799c3c
Vanshika <vanshikav928@gmail.com>
2023-05-10 14:07:56 +0530

    template_string

---
+++ template_string.js
@@ -0,0 +1,8 @@
+let user="Yahoo Baba";
+let marks=350;
+
+let hello = `Hello "${user}" your 'marks' id ${marks}`;
+//document.write(hello);
+console.log(hello);
+
+
---
+++ template_string_function.js
@@ -0,0 +1,9 @@
+let firstName ="Vanshika";
+let lastName="verma";
+
+function fullname(firstName,lastName){
+    return `${firstName} ${lastName}`;
+}
+let hello = `Hello ${fullname(firstName,lastName)}`;
+
+console.log(hello);
